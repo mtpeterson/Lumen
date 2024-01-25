b327266e5a6e51762797105f6125b5dba5524fec
mtpeterson <mtppeterson3212@gmail.com>
2024-01-25 17:12:56 -0500

    Create Lumen.ino

---
+++ Lumen/Lumen.ino
@@ -0,0 +1,14 @@
+#include <Adafruit_NeoPixel.h>
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(151, 6);  // Creates a strand of 151 NeoPixels on Pin 6
+int light = 7;  // The pin to change the mode
+int bright = 8; // The pin to change the brightness
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
