840d8995bc3213949e47ae18312b66dcdfa3b533
mtpeterson <mtppeterson3212@gmail.com>
2024-01-25 17:28:59 -0500

    Starting to make Spark
    The goal is for Spark to be a Class that stores a position of light on a NeoPixel strip. That way in the future I can have multiple Spark objects moving around in whatever pattern I define

---
+++ Lumen/Spark.cpp
@@ -0,0 +1,8 @@
+// Spark.cpp
+// #include <Arduino.h>
+#include "Arduino.h”
+#include "Spark.h"
+
+Spark::Spark(){
+  
+}
---
+++ Lumen/Spark.h
@@ -0,0 +1,18 @@
+// Spark.h
+#ifndef Spark_h
+#define Spark_h
+#include "Arduino.h"
+
+class Spark {
+  public:
+    Spark();
+    void myFunction(int blinkRate);
+
+  private:
+  
+};
+
+
+
+
+#endif
