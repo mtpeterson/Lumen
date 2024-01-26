9e95ea663db143153758eab5de26b18a58c4369e
mtpeterson <mtppeterson3212@gmail.com>
2024-01-25 22:36:24 -0500

    Constructor overloaded
    Used ChatGPT to learn how constructor overloading works in C++. In comparison to Java, you don't need four explicit constructors to do this, and there isn't a super() function. Instead, you can set a default value for the parameter, and if no value is given it will just use the default.

--- Lumen/Spark.cpp
+++ Lumen/Spark.cpp
@@ -3,15 +3,6 @@
 #include "Arduino.h”
 #include "Spark.h"
 
-Spark::Spark(uint32_t _color){
-
-}
-Spark::Spark(uint32_t _color, int position){
-
-}
-Spark::Spark(uint32_t _color, int position, float velocity){
-
-}
 Spark::Spark(uint32_t _color, int position = 0, float velocity = 0, float accel = 0){
 
 }
