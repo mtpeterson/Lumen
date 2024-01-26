5977088bbf553defb17bf805767df9e6cb8bbec2
mtpeterson <mtppeterson3212@gmail.com>
2024-01-25 22:34:38 -0500

    Trying constructor overloading

--- Lumen/Spark.cpp
+++ Lumen/Spark.cpp
@@ -3,6 +3,19 @@
 #include "Arduino.h”
 #include "Spark.h"
 
-Spark::Spark(){
-  
+Spark::Spark(uint32_t _color){
+
+}
+Spark::Spark(uint32_t _color, int position){
+
+}
+Spark::Spark(uint32_t _color, int position, float velocity){
+
+}
+Spark::Spark(uint32_t _color, int position = 0, float velocity = 0, float accel = 0){
+
+}
+
+void Spark::exampleFunction(){
+
 }
--- Lumen/Spark.h
+++ Lumen/Spark.h
@@ -5,11 +5,18 @@
 
 class Spark {
   public:
-    Spark();
-    void myFunction(int blinkRate);
+    Spark(uint32_t _color);
+    Spark(uint32_t _color, int position);
+    Spark(uint32_t _color, int position, float velocity);
+    Spark(uint32_t c_olor, int position, float velocity, float accel);
+    void exampleFunction();
 
   private:
-  
+    int pos;
+    uint32_t color;
+
+    float vel;
+    float acl;
 };
 
 
